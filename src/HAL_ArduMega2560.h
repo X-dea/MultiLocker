5436c9041489b6d56c81ae1525f763e85e03cac0
Jason C.H <ctrysbita@outlook.com>
2017-09-19 23:39:51 +0800

    Support Mega2560
    Add support for Mega2560

---
+++ src/HAL_ArduMega2560.h
@@ -0,0 +1,26 @@
+/**
+* MultiLocker (GPLv3)
+* Support by CTBeta Team http://ctbeta.org/
+* Created by Jason C.H Feb 2017
+*/
+#ifndef HAL_ARDUMEGA2560_H
+#define HAL_ARDUMEGA2560_H
+
+// I2C PORT
+#define PIN_SDA 20
+#define PIN_SCL 21
+
+// SPI PIN
+#define PIN_MISO 50
+#define PIN_MOSI 51
+#define PIN_SCK 52
+#define PIN_SS 53
+
+// IO
+#define PIN_INBUTTON 3 // INDOOR BUTTON
+#define PIN_BUZZER 5   // BUZZER
+#define PIN_RFID_RST 5 // RFID
+#define PIN_LOCK 9     // lOCK
+#define PIN_DETECT 4   // Detector
+
+#endif
